d82c4a8fa788f7fce821157fa55074ebd5fb0a5c
hipotermia <hipotermia@pm.me>
2020-04-29 17:58:40 +0200

    fix

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 AUTH_USERNAME='admin'
 AUTH_PASSWORD='password'
 DOMAIN='yourdomain.com'
-DB_FILE='~/blinder.db'
+DB_FILE='/opt/blinder.db'
--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 source config.py
 sqlite3 $DB_FILE "create table if not exists triggers(
-			id SERIAL PRIMARY KEY,
-			cookies varchar,
-			url varchar,
-			localStorage varchar,
-			sessionStorage varchar,
-			html varchar,
-			canvas varchar,
-			useragent varchar,
-			IP varchar,
-			time timestamp,
-			extra varchar
-			);"
+id SERIAL PRIMARY KEY,
+cookies varchar,
+url varchar,
+localStorage varchar,
+sessionStorage varchar,
+html varchar,
+canvas varchar,
+useragent varchar,
+IP varchar,
+time timestamp,
+extra varchar
+);"
 gunicorn --certfile cert.pem --keyfile key.pem -b 0.0.0.0:443 app:app
